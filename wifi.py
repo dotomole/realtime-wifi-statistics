e2c669c7354d3ae684343959d8e15650ac0d8f96
dotomole <toydinosaurs@hotmail.com>
2019-06-24 13:26:09 +0800

    Initial commit

---
+++ wifi.py
@@ -0,0 +1,62 @@
+import subprocess
+import os
+import sys
+import time
+from tkinter import *
+
+def getDefGateway():
+    cmd = 'ipconfig | findstr /i \"Gateway\"'
+    output = os.popen(cmd).read().split(' ')
+    return output[15]
+
+# Here so it only has to get once for getPing()
+defGateway = getDefGateway()
+
+def getSigStrength():
+    cmd = 'netsh wlan show interfaces | findstr /i \"Signal\"'
+    output = os.popen(cmd).read().split('\n')
+    signal = output[0].split(": ")
+    return signal[1]
+
+def getSSID():
+    cmd = 'netsh wlan show interfaces | findstr /i \"SSID\"'
+    output = os.popen(cmd).read().split('\n')
+    ssid = output[0].split(": ")
+    return ssid[1]
+
+def getPing():
+    try:
+        cmd = 'ping -w 1 /n 1 '+defGateway
+        output = os.popen(cmd).read().split(' ')
+        ping = output[10].split("=")
+        return ping[1]
+    except:
+        return "INF"
+
+def updateSig():
+    sigLabel["text"] = getSigStrength()
+    root.after(100, updateSig)
+
+def updatePing():
+    pingLabel["text"] = getPing()
+    root.after(100, updatePing)
+
+root = Tk()
+labelfont = ('verdana', 30, 'bold')
+
+ssid = Label(root, text=getSSID())
+sigLabel = Label(root, text=getSigStrength())
+pingLabel = Label(root, text=getPing())
+
+ssid.config(font=labelfont, height=2, width=10)
+pingLabel.config(font=labelfont, height=2, width=10)
+sigLabel.config(font=labelfont, height=2, width=10)
+
+ssid.pack()
+sigLabel.pack()
+pingLabel.pack()
+
+root.title('Wi-Fi Statistics')
+root.after(1, updateSig)
+root.after(1, updatePing)
+root.mainloop()
---
+++ wifi_term.py
@@ -0,0 +1,61 @@
+import subprocess
+import os
+import sys
+import time
+
+def main():
+    print("-------------------------------------------------")
+    print("|            Wi-Fi Statistics v0.01             |")
+    print("-------------------------------------------------")
+    print("| This program shows the wi-fi signal strength, |")
+    print("| and ping to the default gateway in realtime.  |")
+    print("-------------------------------------------------")
+    print("Wi-Fi: "+getSSID())
+    display()
+
+def getDefGateway():
+    cmd = 'ipconfig | findstr /i \"Gateway\"'
+    output = os.popen(cmd).read().split(' ')
+    return output[15]
+
+defGateway = getDefGateway()
+
+def getPing():
+    try:
+        cmd = 'ping -w 1 /n 1 '+defGateway
+        output = os.popen(cmd).read().split(' ')
+        ping = output[10].split("=")
+        return ping[1]
+    except:
+        return "INF"
+
+def getSSID():
+    cmd = 'netsh wlan show interfaces | findstr /i \"SSID\"'
+    output = os.popen(cmd).read().split('\n')
+    ssid = output[0].split(": ")
+    return ssid[1]
+
+def getSigStrength():
+    cmd = 'netsh wlan show interfaces | findstr /i \"Signal\"'
+    output = os.popen(cmd).read().split('\n')
+    signal = output[0].split(": ")
+    return signal[1]
+
+# def getSigStrength2():
+#     cmd = 'netsh wlan show networks mode=bssid'
+#     output = os.popen(cmd).read().split('\n')
+#     signal = output[9].split(": ")
+#     return signal[1]
+
+def display():
+    start_time = time.time()
+    while True:
+        sys.stdout.write('\rSignal Strength: %s' %getSigStrength())
+        sys.stdout.write('Ping: %s ' %getPing())
+        elapsed_time = time.time() - start_time
+        sys.stdout.write('Time elapsed: %d seconds   ' %(elapsed_time))
+        time.sleep(0.1)
+        sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
