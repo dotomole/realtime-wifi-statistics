f047078a010aa4b88a5a7d96e7c3215f6c3f742a
dotomole <toydinosaurs@hotmail.com>
2019-08-18 20:31:27 +0800

    Speed in mbps added.

--- README.md
+++ README.md
@@ -2,11 +2,10 @@
 
 This program allows you to view wi-fi statistics in realtime. Something I just made for myself really, useful for walking around your home/workplace and seeing where the signal is best.
 
-Usage:<br/>
-Terminal version: `py wifi_term.py`<br/>
-GUI version: `py wifi.py`<br/>
+Usage: `py wifi.py`<br/>
 
 Current version shows:
 - SSID
 - Wi-Fi Signal Strength
 - Ping to router (default gateway)
+- Router Wi-Fi Speed (Not Internet Speed)
--- wifi.py
+++ wifi.py
@@ -27,6 +27,12 @@ def getSigStrength():
     signal = output[0].split(": ")
     return signal[1]
 
+def getRouterTransRate():
+    cmd = 'netsh wlan show interfaces | findstr /i \"Transmit rate\"'
+    output = os.popen(cmd).read().split('\n')
+    trans = output[0].split(": ")
+    return trans[1]
+
 # Alternative method for signal strength
 # Gives seemingly less stable results
 
@@ -59,13 +65,17 @@ def updatePing():
     pingLabel["text"] = getPing()
     root.after(100, updatePing)
 
+def updateTrans():
+    transLabel["text"] = getRouterTransRate()+"mbps"
+    root.after(100, updateTrans)
+
 root = Tk()
 root.title('Router Wi-Fi Statistics')
-root.geometry("300x400")
+root.geometry("300x500")
 root.resizable(False, False)
 
 titleFont = ('verdana', 10)
-resultFont = ('verdana', 30, 'bold')
+resultFont = ('verdana', 25, 'bold')
 
 if connected():
     defGateway = getDefGateway()
@@ -73,26 +83,37 @@ if connected():
     ssidTitle = Label(root, text="SSID")
     sigTitle = Label(root, text="Signal Strength")
     pingTitle = Label(root, text="Ping to "+defGateway+"(Router)")
+    transTitle = Label(root, text="Router Wi-Fi Speed")
+   
     ssid = Label(root, text=getSSID())
     sigLabel = Label(root, text=getSigStrength())
     pingLabel = Label(root, text=getPing())
+    transLabel = Label(root, text=getRouterTransRate())
 
     ssidTitle.config(font=titleFont, height=2, width=20)
     sigTitle.config(font=titleFont, height=2, width=20)
     pingTitle.config(font=titleFont, height=2, width=20)
+    transTitle.config(font=titleFont, height=2, width=20)
+
     ssid.config(font=resultFont, height=2, width=10)
     pingLabel.config(font=resultFont, height=2, width=10)
     sigLabel.config(font=resultFont, height=2, width=10)
+    transLabel.config(font=resultFont, height=2, width=10)
 
+    #The layout of GUI
     ssidTitle.pack()
     ssid.pack()
     sigTitle.pack()
     sigLabel.pack()
     pingTitle.pack()
     pingLabel.pack()
+    transTitle.pack()
+    transLabel.pack()
 
+    #Constant updating of details
     root.after(1, updateSig)
     root.after(1, updatePing)
+    root.after(1, updateTrans)
 else:
     disc = Label(root, text="Wi-Fi disconnected.")
     disc.config(font=titleFont, height=2, width=30)
